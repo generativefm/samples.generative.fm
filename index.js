61d724751b031548db2d3c37b79853f53619fc9a
metalex9 <alexbainter@gmail.com>
2019-02-17 15:03:14 -0600

    Prep for publishing

--- index.js
+++ index.js
@@ -2,6 +2,4 @@
 
 const isNode = require('detect-node');
 
-module.exports = isNode
-  ? require('./src/client/node/node')
-  : require('./dist/browser');
+module.exports = isNode ? require('./node') : require('./dist/browser');
